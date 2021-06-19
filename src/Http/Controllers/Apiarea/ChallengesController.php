786135a6aafb9baa66507411a70c4ceeb75b7282
Abdelrahman Omran <Omranic@users.noreply.github.com>
2021-06-19 20:00:20 +0000

    Apply fixes from StyleCI

--- src/Http/Controllers/Apiarea/ChallengesController.php
+++ src/Http/Controllers/Apiarea/ChallengesController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Cortex\Oauth\Http\Controllers\Apiarea;
 
 use Cortex\Foundation\Http\Controllers\AuthorizedController;
-use Cortex\Tags\Models\Tag;
 
 class ChallengesController extends AuthorizedController
 {
