4b85039996f398b82e835ee511859246c6eabbf1
Abdelrahman Omran <me@omranic.com>
2022-02-02 10:37:45 +0400

    Update routes to use class based definitions

--- routes/web/adminarea.php
+++ routes/web/adminarea.php
@@ -2,9 +2,11 @@
 
 declare(strict_types=1);
 
+use Cortex\Oauth\Http\Controllers\Adminarea\ClientsController;
+use Cortex\Oauth\Http\Controllers\Adminarea\AuthorizationController;
+
 Route::domain('{adminarea}')->group(function () {
     Route::name('adminarea.')
-         ->namespace('Cortex\Oauth\Http\Controllers\Adminarea')
          ->middleware(['web', 'nohttpcache', 'can:access-adminarea'])
          ->prefix(route_prefix('adminarea'))->group(function () {
 
@@ -13,46 +15,46 @@ Route::domain('{adminarea}')->group(function () {
 
                 // Authorization process
                  Route::prefix('oauth')->group(function () {
-                     Route::get('authorize')->name('authorize')->uses('AuthorizationController@authorizeRequest');
-                     Route::post('authorize')->name('approve')->uses('AuthorizationController@approve');
-                     Route::delete('authorize')->name('deny')->uses('AuthorizationController@deny');
-                     Route::post('token')->name('token')->uses('AuthorizationController@issueToken');
-                     Route::post('token/refresh')->name('token.refresh')->uses('AuthorizationController@refreshToken');
+                     Route::get('authorize')->name('authorize')->uses([AuthorizationController::class, 'authorizeRequest']);
+                     Route::post('authorize')->name('approve')->uses([AuthorizationController::class, 'approve']);
+                     Route::delete('authorize')->name('deny')->uses([AuthorizationController::class, 'deny']);
+                     Route::post('token')->name('token')->uses([AuthorizationController::class, 'issueToken']);
+                     Route::post('token/refresh')->name('token.refresh')->uses([AuthorizationController::class, 'refreshToken']);
                  });
 
                  // Managing clients, auth codes, and access tokens
-                 Route::match(['get', 'post'], 'clients')->name('clients.index')->uses('ClientsController@index');
-                 Route::get('clients/create')->name('clients.create')->uses('ClientsController@create');
-                 Route::post('clients/create')->name('clients.store')->uses('ClientsController@store');
-                 Route::get('clients/{client}/edit')->name('clients.edit')->uses('ClientsController@edit');
-                 Route::put('clients/{client}/edit')->name('clients.update')->uses('ClientsController@update');
-                 Route::put('clients/{client}')->name('clients.revoke')->uses('ClientsController@revoke');
-                 Route::delete('clients/{client}')->name('clients.destroy')->uses('ClientsController@destroy');
-                 Route::match(['get', 'post'], 'clients/{client}/auth-codes')->name('clients.auth_codes')->uses('ClientsController@authCodes');
-                 Route::match(['get', 'post'], 'clients/{client}/access-tokens')->name('clients.access_tokens')->uses('ClientsController@accessTokens');
+                 Route::match(['get', 'post'], 'clients')->name('clients.index')->uses([ClientsController::class, 'index']);
+                 Route::get('clients/create')->name('clients.create')->uses([ClientsController::class, 'create']);
+                 Route::post('clients/create')->name('clients.store')->uses([ClientsController::class, 'store']);
+                 Route::get('clients/{client}/edit')->name('clients.edit')->uses([ClientsController::class, 'edit']);
+                 Route::put('clients/{client}/edit')->name('clients.update')->uses([ClientsController::class, 'update']);
+                 Route::put('clients/{client}')->name('clients.revoke')->uses([ClientsController::class, 'revoke']);
+                 Route::delete('clients/{client}')->name('clients.destroy')->uses([ClientsController::class, 'destroy']);
+                 Route::match(['get', 'post'], 'clients/{client}/auth-codes')->name('clients.auth_codes')->uses([ClientsController::class, 'authCodes']);
+                 Route::match(['get', 'post'], 'clients/{client}/access-tokens')->name('clients.access_tokens')->uses([ClientsController::class, 'accessTokens']);
              });
 
              Route::name('cortex.auth.')->group(function () {
 
                 // Admin clients Routes
                  Route::name('admins.')->prefix('admins')->group(function () {
-                     Route::get('{admin}/clients')->name('clients')->uses('ClientsController@clientsForUser');
-                     Route::get('{admin}/auth-codes')->name('auth_codes')->uses('ClientsController@authCodesForUser');
-                     Route::get('{admin}/access-tokens')->name('access_tokens')->uses('ClientsController@accessTokensForUser');
+                     Route::get('{admin}/clients')->name('clients')->uses([ClientsController::class, 'clientsForUser']);
+                     Route::get('{admin}/auth-codes')->name('auth_codes')->uses([ClientsController::class, 'authCodesForUser']);
+                     Route::get('{admin}/access-tokens')->name('access_tokens')->uses([ClientsController::class, 'accessTokensForUser']);
                  });
 
                  // Manager clients Routes
                  Route::name('managers.')->prefix('managers')->group(function () {
-                     Route::get('{manager}/clients')->name('clients')->uses('ClientsController@clientsForUser');
-                     Route::get('{manager}/auth-codes')->name('auth_codes')->uses('ClientsController@authCodesForUser');
-                     Route::get('{manager}/access-tokens')->name('access_tokens')->uses('ClientsController@accessTokensForUser');
+                     Route::get('{manager}/clients')->name('clients')->uses([ClientsController::class, 'clientsForUser']);
+                     Route::get('{manager}/auth-codes')->name('auth_codes')->uses([ClientsController::class, 'authCodesForUser']);
+                     Route::get('{manager}/access-tokens')->name('access_tokens')->uses([ClientsController::class, 'accessTokensForUser']);
                  });
 
                  // Member clients Routes
                  Route::name('members.')->prefix('members')->group(function () {
-                     Route::get('{member}/clients')->name('clients')->uses('ClientsController@clientsForUser');
-                     Route::get('{member}/auth-codes')->name('auth_codes')->uses('ClientsController@authCodesForUser');
-                     Route::get('{member}/access-tokens')->name('access_tokens')->uses('ClientsController@accessTokensForUser');
+                     Route::get('{member}/clients')->name('clients')->uses([ClientsController::class, 'clientsForUser']);
+                     Route::get('{member}/auth-codes')->name('auth_codes')->uses([ClientsController::class, 'authCodesForUser']);
+                     Route::get('{member}/access-tokens')->name('access_tokens')->uses([ClientsController::class, 'accessTokensForUser']);
                  });
              });
          });
