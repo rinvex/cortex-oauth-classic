73b74698c9088d356ddb6769307ad2a0d3e02fec
Abdelrahman Omran <me@omranic.com>
2022-02-02 10:38:40 +0400

    Fix broadcasts naming convensions

--- routes/broadcasts/channels.php
+++ routes/broadcasts/channels.php
@@ -6,48 +6,48 @@ use Illuminate\Contracts\Auth\Access\Authorizable;
 
 Broadcast::channel('cortex.oauth.clients.index', function (Authorizable $user) {
     return $user->can('list', app('rinvex.oauth.client'));
-}, ['guards' => ['admin']]);
+});
 
 Broadcast::channel('cortex.oauth.clients.auth_codes', function (Authorizable $user) {
     return $user->can('list', app('rinvex.oauth.auth_code'));
-}, ['guards' => ['admin']]);
+});
 
 Broadcast::channel('cortex.oauth.clients.access_tokens', function (Authorizable $user) {
     return $user->can('list', app('rinvex.oauth.access_token'));
-}, ['guards' => ['admin']]);
+});
 
 Broadcast::channel('cortex.auth.admins.index', function (Authorizable $user) {
     return $user->can('list', app('rinvex.oauth.client'));
-}, ['guards' => ['admin']]);
+});
 
 Broadcast::channel('cortex.auth.admins.auth_codes', function (Authorizable $user) {
     return $user->can('list', app('rinvex.oauth.auth_code'));
-}, ['guards' => ['admin']]);
+});
 
 Broadcast::channel('cortex.auth.admins.access_tokens', function (Authorizable $user) {
     return $user->can('list', app('rinvex.oauth.access_token'));
-}, ['guards' => ['admin']]);
+});
 
 Broadcast::channel('cortex.auth.manager.index', function (Authorizable $user) {
     return $user->can('list', app('rinvex.oauth.client'));
-}, ['guards' => ['admin']]);
+});
 
 Broadcast::channel('cortex.auth.manager.auth_codes', function (Authorizable $user) {
     return $user->can('list', app('rinvex.oauth.auth_code'));
-}, ['guards' => ['admin']]);
+});
 
 Broadcast::channel('cortex.auth.manager.access_tokens', function (Authorizable $user) {
     return $user->can('list', app('rinvex.oauth.access_token'));
-}, ['guards' => ['admin']]);
+});
 
 Broadcast::channel('cortex.auth.members.index', function (Authorizable $user) {
     return $user->can('list', app('rinvex.oauth.client'));
-}, ['guards' => ['admin']]);
+});
 
 Broadcast::channel('cortex.auth.members.auth_codes', function (Authorizable $user) {
     return $user->can('list', app('rinvex.oauth.auth_code'));
-}, ['guards' => ['admin']]);
+});
 
 Broadcast::channel('cortex.auth.members.access_tokens', function (Authorizable $user) {
     return $user->can('list', app('rinvex.oauth.access_token'));
-}, ['guards' => ['admin']]);
+});
